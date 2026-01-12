9cd4ca194955e44e71d7e878e2f35a29f89a9584
Nolan Rousseau <nolanr.contact@gmail.com>
2026-01-12 22:20:40 +0100

    manage account html

--- src/routes/(authed)/manage_account/+page.svelte
+++ src/routes/(authed)/manage_account/+page.svelte
@@ -1,1 +1,140 @@
-<h1 class="text-3xl font-bold">Gestion du compte</h1>
+<script>
+    let passwordInput;
+    let showPassword = false;
+
+    function togglePassword() {
+        showPassword = !showPassword;
+    }
+</script>
+
+<head>
+	<title>Gestion du compte - [nom du jeu]</title>
+</head>
+
+<div class="min-h-90 flex flex-col items-center justify-center p-6">
+    <main class="w-full max-w-lg p-8 mt-20 mb-20 rounded-tl-3xl rounded-br-3xl bg-white/10 border border-white/6 shadow-lg backdrop-blur-md">
+        <div class="flex items-center gap-4 mb-10">
+            <div>
+                <h1 class="text-3xl font-semibold text-white">Gestion du compte</h1>
+                <p class="text-sm text-sky-200">Modifiez vos informations.</p>
+            </div>
+        </div>
+        
+        <form method="post" class="space-y-4">
+            <fieldset class="space-y-4 border-0 p-0">
+                <legend class="sr-only">Créer votre compte</legend>
+                <label for="username" class="block text-md mb-1 font-bold text-white">Nom d'utilisateur*</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    autocomplete="username"
+                    minlength="3"
+                    maxlength="20"
+                    autocapitalize="none"
+                    required
+
+                    value="username_test"
+                    class="w-full rounded-lg 
+                    bg-white/3 border border-white/6 text-white placeholder-sky-200/50 
+                    focus:outline-none focus:ring-2 focus:ring-orange-400"
+                />
+                <div class="relative mb-10">
+                    <p id="username-help" class="absolute right-2 -top-5 text-xs 
+                            px-1 py-1 rounded-md text-white">
+                        Entre 3 et 20 caractères, sans espaces.
+                    </p>
+                </div>
+        
+                <label for="username_linkedin" class="block text-md mb-1 font-bold text-white">Nom d'utilisateur Linkedin</label>
+                <input
+                    type="text"
+                    id="username_linkedin"
+                    name="username_linkedin"
+                    autocomplete="username"
+                    autocapitalize="none"
+                    
+                    value="username_linkedin_test"
+                    class="w-full rounded-lg 
+                    bg-white/3 border border-white/6 text-white placeholder-sky-200/50 
+                    focus:outline-none focus:ring-2 focus:ring-orange-400"
+                />
+                
+                <label for="email" class="block text-md mb-1 font-bold text-white">Email*</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="mail"
+                    autocomplete="email"
+                    required
+        
+                    value="email_test@gmail.com"
+                    class="w-full rounded-lg 
+                    bg-white/3 border border-white/6 text-white placeholder-sky-200/50 
+                    focus:outline-none focus:ring-2 focus:ring-orange-400"
+                />
+
+        
+                <label for="password" class="block text-md mb-1 font-bold text-white">Nouveau mot de passe</label>
+                <div class="relative mb-10">
+                    <input
+                        bind:this={passwordInput}
+                        type={showPassword ? 'text' : 'password'}
+                        id="password"
+                        name="password"
+                        autocomplete="new-password"
+                        value=""
+                        class="w-full rounded-lg 
+                        bg-white/3 border border-white/6 text-white placeholder-sky-200/50 
+                        focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    />
+                    <button
+                        type="button"
+                        aria-pressed={showPassword}
+                        on:click={togglePassword}
+                        class="absolute right-2 top-1/2 -translate-y-1/2 text-sm 
+                        bg-white/6 px-3 py-1 rounded-md text-white hover:bg-white/20"
+                    >
+                        {showPassword ? 'Masquer le mot de passe' : 'Afficher le mot de passe'}
+                    </button>
+                </div>
+        
+                <label for="password" class="block text-md mb-1 font-bold text-white">Confirmez le nouveau mot de passe</label>
+                <div class="relative mb-10">
+                    <input
+                        type='password'
+                        id="password_confirm"
+                        name="password_confirm"
+                        autocomplete="new-password"
+                        value=""
+                        class="w-full rounded-lg 
+                        bg-white/3 border border-white/6 text-white placeholder-sky-200/50 
+                        focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    />
+                </div>
+        
+                <div class="flex flex-row items-center justify-end mb-10">
+                    <button
+                        type="submit"
+                        class="bg-gradient-to-br from-emerald-400 via-indigo-400 to-indigo-500 
+                        font-semibold px-5 py-2 rounded-lg shadow 
+                        hover:bg-gradient-to-br hover:from-indigo-400 hover:via-indigo-500 hover:to-indigo-600"
+                    >
+                        Valider les modifications   
+                    </button>
+                </div>
+            </fieldset>
+        </form>
+        <a href="/vessel" class="text-sm text-white underline hover:text-sky-200">
+            Annuler les modifications
+        </a>
+        <form method="POST" action="/delete-account">
+            <button class="bg-gradient-to-br from-red-400 via-red-400 to-red-500 
+                font-semibold px-5 py-2 rounded-lg shadow 
+                hover:bg-gradient-to-br hover:from-red-400 hover:via-red-500 hover:to-red-600"
+            >
+                Supprimer le compte
+            </button>
+        </form>
+    </main>
+</div>
